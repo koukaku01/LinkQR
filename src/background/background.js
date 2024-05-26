c7f3360d17d79a440db568f23bbda02136d35e76
xKe-desktop <148967184+xKe00@users.noreply.github.com>
2024-05-26 17:14:31 +0100

    [tweak]: update pageAction icon

--- src/background/background.js
+++ src/background/background.js
@@ -1,26 +1,26 @@
 // Listen for tab updates
 browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    // Show the page action on every tab
-    browser.pageAction.show(tabId);
+  // Show the page action on every tab
+  browser.pageAction.show(tabId);
+});
+
+// Listen for tab switching
+browser.tabs.onActivated.addListener(activeInfo => {
+  // Get the current active tab
+  browser.tabs.get(activeInfo.tabId).then(tab => {
+    // Show the page action on the active tab
+    browser.pageAction.show(tab.id);
   });
-  
-  // Listen for tab switching
-  browser.tabs.onActivated.addListener(activeInfo => {
-    // Get the current active tab
-    browser.tabs.get(activeInfo.tabId).then(tab => {
-      // Show the page action on the active tab
-      browser.pageAction.show(tab.id);
-    });
-  });
+});
 
 // Create a browser menu item with id "open-popup"
 browser.menus.create({
-  id: "open-popup",                    // Unique identifier for the menu item
-  title: "Open LinkQR",                // Text displayed for the menu item
-  contexts: ["all"],                   // Menu item appears in all contexts
+id: "open-popup",                    // Unique identifier for the menu item
+title: "Open LinkQR",                // Text displayed for the menu item
+contexts: ["all"],                   // Menu item appears in all contexts
 });
 
 // Add a listener for when the menu item is clicked
 browser.menus.onClicked.addListener(() => {
-  browser.pageAction.openPopup();      // Open the page action popup
+browser.pageAction.openPopup();      // Open the page action popup
 });
